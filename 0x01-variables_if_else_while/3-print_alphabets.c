69503d3536e97e6e7db7e970bd699ed525da1bed
Oke253 <salahudeenfataioke@gmail.com>
2022-10-16 14:10:16 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return 0: no error, non zero if error.
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+		putchar('\n');
+
+		return (0);
+}
