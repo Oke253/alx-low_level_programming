f1c24fe184addae7a1ea2e9a3f98a4850b1c7657
Oke253 <salahudeenfataioke@gmail.com>
2022-10-16 14:36:05 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Return:0 no error, non zero if erro
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '0'; i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+	return (0);
+}
